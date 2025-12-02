754907f1d33b251371bf961a4cb845a28c7869b1
Illia Dobrovolskyi <illia.dobrovolskyi2@gmail.com>
2025-12-02 14:57:59 +0200

    implemented BookDao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,41 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book = new Book();
+        book.setTitle("Head First Java");
+        book.setPrice(BigDecimal.valueOf(14.50));
+
+        Book createdBook = bookDao.create(book);
+        System.out.println("Created a book: " + createdBook);
+
+        List<Book> allBooks = bookDao.findAll();
+        System.out.println("Books list: " + allBooks);
+
+        Optional<Book> bookById = bookDao.findById(book.getId());
+        System.out.println("Found book by id: " + book.getId() + ". The book: " + bookById);
+
+        Book newBook = new Book();
+        newBook.setId(1L);
+        newBook.setTitle("Java 8 in action");
+        newBook.setPrice(BigDecimal.valueOf(12.99));
+
+        Book updatedBook = bookDao.update(newBook);
+        System.out.println("Updated book: " + updatedBook);
+
+        boolean isDeleted = bookDao.deleteById(book.getId());
+        System.out.println("The book by id - " + book.getId() + " is deleted: " + isDeleted);
     }
 }
